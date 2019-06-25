4c0312babba31f124b599b3b1851d3ecd2520349
TeddyYin <ltboegod143@gmail.com>
2019-06-25 16:13:34 +0800

    take photos

--- TesseractMVC/Controllers/TesseractController.cs
+++ TesseractMVC/Controllers/TesseractController.cs
@@ -17,20 +17,50 @@ namespace TesseractMVC.Controllers
         {
             var img = new Bitmap(@"D:\aaa.png");
 
+            #region get in bin folder path
             var path = Path.GetDirectoryName(Assembly.GetExecutingAssembly().CodeBase);
             path = Path.Combine(path, "tessdata");
             path = path.Replace("file:\\", "");
+            #endregion
 
             var ocr = new TesseractEngine(path, "eng", EngineMode.TesseractAndCube);
 
             string str = ocr.Process(img).GetText();
-
-            //ViewBag.Message = "Your contact page.";
 
             ViewBag.Message = str;
 
             return View();
         }
         #endregion
+
+        //public void ProcessRequest(HttpContext context)
+        //{
+        //    context.Response.ContentType = "text/plain";
+        //    string base64String = context.Request["data"];
+        //    Image img = this.Base64ToImage(base64String);
+        //    img.Save(@"D:\ccc.jpg");//儲存圖片
+        //}
+
+        //public bool IsReusable
+        //{
+        //    get
+        //    {
+        //        return false;
+        //    }
+        //}
+
+        ////把base64字串轉成Image物件
+        //public Image Base64ToImage(string base64String)
+        //{
+        //    // Convert Base64 String to byte[]
+        //    byte[] imageBytes = Convert.FromBase64String(base64String);
+        //    MemoryStream ms = new MemoryStream(imageBytes, 0,
+        //      imageBytes.Length);
+
+        //    // Convert byte[] to Image
+        //    ms.Write(imageBytes, 0, imageBytes.Length);
+        //    Image image = Image.FromStream(ms, true);
+        //    return image;
+        //}
     }
 }
